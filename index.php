fb41c514c05ea108de20132a7c3a6fb8bcd8d87e
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2024-03-14 22:54:11 +0700

    all
    all

---
+++ index.php
@@ -0,0 +1,4 @@
+<form name="form" action="proses.php" method="GET">
+    Nilai : <input type="text" name="angka"> <br>
+            <input type="submit" value="SUBMIT" >
+</form>
---
+++ proses.php
@@ -0,0 +1,6 @@
+File proses <hr>
+
+<?php
+$nilai = $_GET["angka"] ;
+echo "Nilai Anada = ". $nilai . "<br/>"
+?> 
